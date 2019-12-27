c6e68a8d88fbff7379edf692648fd661d0c6a2fb
jgustavoas <jgustavoas@gmail.com>
2019-12-27 10:44:28 -0300

    Ajuste no intervalo

--- gympointAPI/src/app/controllers/CheckinController.js
+++ gympointAPI/src/app/controllers/CheckinController.js
@@ -67,7 +67,7 @@ class CheckinController {
       const quantosCheckins = checkins.length;
 
       if (quantosCheckins === 5) {
-        const intervalo4 = eachDayOfInterval({
+        /* const intervalo4 = eachDayOfInterval({
           start: checkins[4].createdAt,
           end: checkins[3].createdAt,
         });
@@ -82,16 +82,22 @@ class CheckinController {
         const intervalo1 = eachDayOfInterval({
           start: checkins[1].createdAt,
           end: checkins[0].createdAt,
+        }); */
+
+        const intervalo = eachDayOfInterval({
+          start: checkins[4].createdAt,
+          end: checkins[0].createdAt,
         });
 
         if (
-          intervalo4.length <= 2 &&
-          intervalo3.length <= 2 &&
-          intervalo2.length <= 2 &&
-          intervalo1.length <= 2
+          /* intervalo4.length <= 7 &&
+          intervalo3.length <= 7 &&
+          intervalo2.length <= 7 &&
+          intervalo1.length <= 7 */
+          intervalo.length <= 7
         ) {
           return res.status(200).json({
-            impedido: `Você já fez 5 checkins dentro de cinco dias seguidos!`,
+            impedido: `Você já fez 5 checkins dentro sete dias!`,
           });
         }
       }
