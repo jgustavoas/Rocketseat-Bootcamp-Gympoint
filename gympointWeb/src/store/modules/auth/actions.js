7549c54d8bc5c3cca15b2694719c32afbd4fe53a
Gustavo Alexandrino <jgustavoas@gmail.com>
2019-12-31 12:46:38 -0300

    Primeiro commit da aplicação web

--- gympointWeb/src/store/modules/auth/actions.js
+++ gympointWeb/src/store/modules/auth/actions.js
@@ -1,21 +1,21 @@
 export function signUpRequest(name, email, password) {
   return {
     type: '@auth/SIGN_UP_REQUEST',
-    payload: { name, email, password }, // Método do Diego para armazenar os dados da request num objeto que ele chama de "payload"
+    payload: { name, email, password }, // Armazenar os dados da request num objeto chamdado de "payload"
   };
 }
 
 export function signInRequest(email, password) {
   return {
     type: '@auth/SIGN_IN_REQUEST',
-    payload: { email, password }, // Método do Diego para armazenar os dados da request num objeto que ele chama de "payload"
+    payload: { email, password }, // Armazenar os dados da request num objeto chamdado de "payload"
   };
 }
 
 export function signInSuccess(token, user) {
   return {
     type: '@auth/SIGN_IN_SUCCESS',
-    payload: { token, user },
+    payload: { token, user }, // Armazenar os dados da request num objeto chamdado de "payload"
   };
 }
 
